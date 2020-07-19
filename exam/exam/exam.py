22454484931967ec317a9bcc994f3c713b0c2af0
Jettio <55096358+Jettio@users.noreply.github.com>
2020-07-19 13:00:59 +0300

    Add files via upload

---
+++ exam/exam/exam.py
@@ -0,0 +1,13 @@
+
+def function(ninjas):
+    if ninjas<50:
+        print("Их слишком много")
+    else:
+        if ninjas<30:
+            print("Будет непросто, но я с ними разделаюсь")
+        else:
+            if ninjas<10:
+                print("Я одолею этих ниндзя!")
+    
+
+param = function(5)
