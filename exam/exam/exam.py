7d01648ffd46d8b8d278400291721958bb106dfa
Jettio <55096358+Jettio@users.noreply.github.com>
2020-07-19 13:17:08 +0300

    Only If

--- exam/exam/exam.py
+++ exam/exam/exam.py
@@ -2,12 +2,10 @@
 def function(ninjas):
     if ninjas<50:
         print("Их слишком много")
-    else:
-        if ninjas<30:
-            print("Будет непросто, но я с ними разделаюсь")
-        else:
-            if ninjas<10:
-                print("Я одолею этих ниндзя!")
+    if ninjas<30:
+        print("Будет непросто, но я с ними разделаюсь")
+    if ninjas<10:
+        print("Я одолею этих ниндзя!")
     
 
 param = function(5)
